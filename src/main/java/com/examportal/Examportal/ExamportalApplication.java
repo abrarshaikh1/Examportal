6e800155bece307fa86fe8cc63edb40385fc7810
Abrar <84625091+abrarshaikh1@users.noreply.github.com>
2024-10-19 19:06:13 +0530

    added cross configuration

--- src/main/java/com/examportal/Examportal/ExamportalApplication.java
+++ src/main/java/com/examportal/Examportal/ExamportalApplication.java
@@ -2,12 +2,34 @@ package com.examportal.Examportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExamportalApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ExamportalApplication.class, args);
 	}
 
-}
+	@Bean
+	public WebMvcConfigurer configureCors(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Allow CORS on all endpoints
+						.allowedOrigins("http://localhost:4200") //Allow Angular app origin
+						.allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT") // Allowed HTTP methods
+						.allowedHeaders("*") // Allow all headers
+						.allowCredentials(true) // Allow credentials (cookies, etc.)
+						.maxAge(3600); // Preflight request cache duration
+			}
+
+
+			};
+		}
+	}
+
+
