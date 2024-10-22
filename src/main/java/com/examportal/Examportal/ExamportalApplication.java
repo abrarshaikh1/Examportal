71e4e037833f889ea5819e3a5dd6d5001bc9de32
Abrar <84625091+abrarshaikh1@users.noreply.github.com>
2024-10-23 00:29:35 +0530

    added the swagger and map struct

--- src/main/java/com/examportal/Examportal/ExamportalApplication.java
+++ src/main/java/com/examportal/Examportal/ExamportalApplication.java
@@ -1,12 +1,17 @@
 package com.examportal.Examportal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
+@OpenAPIDefinition(info = @Info(title = "Spring Boot API", version = "1.0", description = "API documentation for the Spring Boot project"))
 public class ExamportalApplication {
 
 	public static void main(String[] args) {
